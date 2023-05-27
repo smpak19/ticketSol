210afe25679401fb5fcc4e31f16b9cd073acf857
smpak19 <smpak@kaist.ac.kr>
2023-05-27 18:50:25 +0900

    DONE

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,12 +5,19 @@ async function main() {
 
   console.log(deployer);
 
+  
   const TicketNFTFactory = await ethers.getContractFactory("TicketNFTFactory");
+  const TicketNFT = await ethers.getContractFactory("TicketNFT");
   const ticketNFTFactory = await TicketNFTFactory.deploy();
+  const ticketNFT = await TicketNFT.deploy("", 0, 0, "");
 
   await ticketNFTFactory.deployed();
+  await ticketNFT.deployed();
 
+  // npx hardhat verify {address}
   console.log(`TicketNFTFactory deployed to ${ticketNFTFactory.address}`);
+  // npx hardhat verify {address} --constructor-args arguments.js
+  console.log(`Dummy ticketNFT deployed to ${ticketNFT.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
